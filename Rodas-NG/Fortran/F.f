deeb33ffbe8ed2e8405baaaaf8268073f2677002
Thierry Dumont <tdumont@math.univ-lyon1.fr>
2017-10-10 16:14:07 +0200

    suppress fortran

--- Rodas-NG/Fortran/F.f
+++
@@ -1,9 +0,0 @@
-
-         subroutine F(N,X,Y,R,RPAR,IPAR)
-         implicit none
-         double precision Y(3),R(3),X,RPAR
-         integer N,IPAR
-         r(1)=77.27d0*(y(2)+y(1)*(1.-8.375d-06*y(1)-y(2)));
-         r(2)=(y(3)-(1.+y(1))*y(2))/77.27d0;
-         r(3)=0.161d0*(y(1)-y(3));
-         end
